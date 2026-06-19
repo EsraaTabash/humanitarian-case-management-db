728c8f067595559daf91aa7e7f1a7f47fce31634
Esraa Mazen Tabash <125120036+EsraaTabash@users.noreply.github.com>
2026-06-19 12:12:00 +0300

    Update 01_business_functions.sql

--- 03_functions/01_business_functions.sql
+++ 03_functions/01_business_functions.sql
@@ -7,20 +7,16 @@ CREATE FUNCTION fn_GetRiskLevel
 RETURNS NVARCHAR(20)
 AS
 BEGIN
-
-    DECLARE @RiskLevel NVARCHAR(20)
+    DECLARE @RiskLevel NVARCHAR(20);
 
     IF @Score >= 80
-        SET @RiskLevel = 'High'
-
+        SET @RiskLevel = 'High';
     ELSE IF @Score >= 50
-        SET @RiskLevel = 'Medium'
-
+        SET @RiskLevel = 'Medium';
     ELSE
-        SET @RiskLevel = 'Low'
+        SET @RiskLevel = 'Low';
 
-    RETURN @RiskLevel
-
+    RETURN @RiskLevel;
 END;
 GO
 
@@ -35,14 +31,49 @@ CREATE FUNCTION fn_HouseholdPriority
 RETURNS DECIMAL(5,2)
 AS
 BEGIN
-
-    DECLARE @Priority DECIMAL(5,2)
+    DECLARE @Priority DECIMAL(5,2);
 
     SET @Priority =
         (@FamilySize * 2)
-        + (@VulnerabilityScore * 0.8)
+        + (@VulnerabilityScore * 0.8);
 
-    RETURN @Priority
+    RETURN @Priority;
+END;
+GO
 
+
+-- Beneficiary Age Category
+
+CREATE FUNCTION fn_GetAgeCategory
+(
+    @Age INT
+)
+RETURNS NVARCHAR(20)
+AS
+BEGIN
+    DECLARE @Category NVARCHAR(20);
+
+    IF @Age < 18
+        SET @Category = 'Child';
+    ELSE IF @Age < 60
+        SET @Category = 'Adult';
+    ELSE
+        SET @Category = 'Elderly';
+
+    RETURN @Category;
+END;
+GO
+
+
+-- Days Since Household Registration
+
+CREATE FUNCTION fn_DaysSinceRegistration
+(
+    @RegistrationDate DATE
+)
+RETURNS INT
+AS
+BEGIN
+    RETURN DATEDIFF(DAY, @RegistrationDate, GETDATE());
 END;
 GO
